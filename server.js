b4b5b16c9768b2bcf4b9ceef44af3fa72f037add
mohamed-anwar <mohamed_anwar@opmbx.org>
2016-10-30 12:35:52 +0200

    added application

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express()
+
+var PORT = process.env.PORT || 8080;
+
+app.get('/', function(req, res) {
+	res.send('Hello, World!');
+});
+
+app.listen(PORT);
+console.log("Started on localhost:" + PORT);
